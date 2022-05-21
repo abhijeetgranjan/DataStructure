5c2e75965b09eb0f9ba23811771afab6917ba266
Abhi <abhiran6264@gmail.com>
2022-05-21 21:22:06 +0530

    Added methods add to last and searching

--- src/main/java/doublylinkedlist/DoublyLinkedList.java
+++ src/main/java/doublylinkedlist/DoublyLinkedList.java
@@ -2,37 +2,75 @@ package main.java.doublylinkedlist;
 
 public class DoublyLinkedList {
 
-    private DoublyNode head, tail,curr;
+    private DoublyNode head, tail, curr;
     private int size;
 
-    public void addToFront(int value){
+    public void addToFront(int value) {
         DoublyNode node = new DoublyNode(value);
-        if(head==null){
-            head= node;
-            tail=node;
-        }else{
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
             head.setPrev(node);
-            head= node;
+            head = node;
             head.setNext(curr);
         }
-        curr=node;
+        curr = node;
         size++;
     }
 
-    public void sizeOfList(){
-        System.out.println(" the size of list is "+size);
+    public void addToLast(int value) {
+        DoublyNode node = new DoublyNode(value);
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.setNext(node);
+            tail = node;
+            tail.setPrev(curr);
+        }
+        size++;
+        curr = node;
     }
 
-    public void printList(){
-        if(head==null){
+    public void sizeOfList() {
+        System.out.println(" the size of list is " + size);
+    }
+
+    public void printList() {
+        if (head == null) {
             System.out.println(" No element in the list");
-        }else{
+        } else {
             curr = head;
             System.out.print(curr.getValue());
-            while(curr.getNext()!=null){
-                curr=curr.getNext();
-                System.out.print(" --> "+curr.getValue());
+            while (curr.getNext() != null) {
+                curr = curr.getNext();
+                System.out.print(" --> " + curr.getValue());
             }
         }
+    }
+
+    public void getIndex(int value) {
+
+        int index = 1;
+        if (head == null) {
+            System.out.println(" the list is empty");
+        } else {
+            curr = head;
+            if (curr.getValue() == value) {
+                System.out.println(" the element " + value + " is present at index " + index);
+            } else {
+                while (curr!= null) {
+
+                    if (curr.getValue() == value) {
+                        System.out.println(" the element " + value + " is present at index " + index);
+                        return;
+                    }index++;
+                    curr = curr.getNext();
+                }
+                System.out.println(" the element "+ value+" is not present in the list");
+            }
+        }
+
     }
 }
--- src/main/java/doublylinkedlist/DoublyLinkedListMain.java
+++ src/main/java/doublylinkedlist/DoublyLinkedListMain.java
@@ -3,13 +3,29 @@ package main.java.doublylinkedlist;
 public class DoublyLinkedListMain {
     public static void main(String[] args) {
 
+
         DoublyLinkedList linkedList= new DoublyLinkedList();
+
         linkedList.addToFront(5);
         linkedList.addToFront(6);
         linkedList.addToFront(8);
 
         linkedList.sizeOfList();
-
         linkedList.printList();
+
+
+        linkedList.addToLast( 9);
+        linkedList.addToLast(10);
+        linkedList.addToLast(11);
+
+        System.out.println(" ");
+        linkedList.sizeOfList();
+        linkedList.printList();
+
+        System.out.println(" ");
+        linkedList.getIndex(89);
+        linkedList.getIndex(8);
+        linkedList.getIndex(9);
+
     }
 }
