40456d1776df211a922c89e46ceb93942ae65b0a
Abhi <abhiran6264@gmail.com>
2022-07-23 21:30:25 +0530

    Bubble Sort

--- src/main/java/sorting/BubbleSort.java
+++ src/main/java/sorting/BubbleSort.java
@@ -1,4 +1,10 @@
 package main.java.sorting;
+/*
+Bubble sort is a sorting method where we compare each element one by one and push the largest element at end and
+then do the same process for the element from index to size n-1
+{8, 3, 9, 7, 5, 4, -10} --> { 3,8,7,5,4,-10,9} --> {3,7,5,4,-10,8,9} --> {3,5,4,-10,7,8,9} -->{3,4,-10,5,7,8,9}
+--> {3,-10,4,5,7,8,9} -->{-10,3,4,5,7,8,9}
+ */
 
 public class BubbleSort {
     public static void main(String[] args) {
