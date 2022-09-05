64a1da7da54c12e2f219e35df380d25c90de93ac
Abhi <abhiran6264@gmail.com>
2022-09-05 18:43:24 +0530

    Counting sort

---
+++ src/main/java/sorting/CountingSort.java
@@ -0,0 +1,36 @@
+package main.java.sorting;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class CountingSort {
+
+    public static void main(String[] args) {
+        int[] array = {0,1,2,0,0,1,2,1,1,2};
+        countingSortImpl(array, Arrays.stream(array).min(), Arrays.stream(array).max());
+
+        System.out.println();
+        int[] array1 = {0, 1, 2, 8, 9,5, 6};
+        countingSortImpl(array1, Arrays.stream(array1).min(), Arrays.stream(array1).max());
+    }
+
+    private static void countingSortImpl(int[] array, OptionalInt min, OptionalInt max) {
+        int []tempArray = new  int[max.getAsInt()-min.getAsInt()+1];
+
+        for(int i: array){
+            tempArray[i]++;
+        }
+
+        int j=0;
+        for (int i = min.getAsInt(); i <= max.getAsInt(); i++) {
+            while (tempArray[i-min.getAsInt()]>0){
+             array[j]=i;
+             tempArray[i]--;
+             j++;
+            }
+        }
+        Arrays.stream(array).forEach(i -> System.out.print(i+" "));
+    }
+
+
+}
