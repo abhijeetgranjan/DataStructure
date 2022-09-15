7890a14fdf907c084ee10f8aaafa5696be9ba0b4
Abhi <abhiran6264@gmail.com>
2022-09-15 12:28:42 +0530

    Implementing queue using array

--- src/main/java/queue/QueueImpl.java
+++ src/main/java/queue/QueueImpl.java
@@ -1,36 +1,59 @@
 package main.java.queue;
 
+import java.util.NoSuchElementException;
+
 public class QueueImpl {
 
     private int[] queue;
     private int top;
-    private int front=0;
+    private int front = 0;
 
-    QueueImpl(int capacity){
+    QueueImpl(int capacity) {
         queue = new int[capacity];
     }
 
-    public void add (int value){
-        if(top == queue.length-1){
+    public void add(int value) {
+        if (top == queue.length) {
             System.out.println("queue is full ");
+            return;
         }
         queue[top++] = value;
     }
 
-    public int remove(){
+    public int remove() {
+        if (isEmpty()) {
+            System.out.println(" queue is empty");
+            return -1;
+        }
+
         int removedElement = queue[front];
         front++;
+        if(isEmpty()){
+            top=0;
+            front=0;
+        }
         return removedElement;
     }
 
-    public int peek(){
+    public boolean isEmpty() {
+        return top - front == 0;
+    }
+
+    public int getSize(){
+        return  top- front;
+    }
+
+    public int peek() {
+        if(isEmpty()){
+            throw new NoSuchElementException();
+        }
         return queue[front];
     }
 
-    public void print(){
+    public void print() {
         for (int i = front; i < top; i++) {
             System.out.print("| ");
-            System.out.print(queue[i] +" |");
+            System.out.print(queue[i] + " |");
         }
         System.out.println();
     }
--- src/main/java/queue/QueueMain.java
+++ src/main/java/queue/QueueMain.java
@@ -11,13 +11,28 @@ public class QueueMain {
         queue.add(4);
         queue.add(5);
         queue.add(6);
+        queue.add(7);
+        queue.add(8);
+        queue.add(9);
+        queue.add(10);
 
         queue.print();
 
         queue.remove();
         queue.remove();
-        System.out.println(queue.peek());
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.remove();
+        queue.remove();
 
+        //System.out.println(queue.peek());
+        queue.add(11);
+        System.out.println(queue.getSize());
         queue.print();
+
     }
 }
